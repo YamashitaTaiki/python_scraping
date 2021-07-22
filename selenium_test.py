548927a63a31137e0cab49193da71c2b9b4e4218
root <root@v133-18-53-227.vir.kagoya.net>
2021-07-23 02:58:26 +0900

    mod_chromeDriver

--- selenium_test.py
+++ selenium_test.py
@@ -1,33 +1,39 @@
 from selenium import webdriver
 import sys
+from selenium.webdriver.chrome.options import Options
 
 args = sys.argv
 USER = "tai.tai.kinntyann.so@gmail.com"
 PASS = args[1]
 print(PASS)
-# PhantomJSのドライバーを得る
-browser = webdriver.PhantomJS()
-browser.implicitly_wait(3)
+
+# ドライバーを得る
+options = webdriver.ChromeOptions()
+options.add_argument('--disable-gpu')
+options.add_argument("--no-sandbox")
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+driver.get('https://google.com')
 
 # ログインページにアクセス
 url_login = "https://1point02.jp/op/login.aspx"
-browser.get(url_login)
+driver.get(url_login)
 print("ログインページにアクセスしました")
 
 # テキストボックスに文字を入力
-e = browser.find_element_by_id("edtID")
+e = driver.find_element_by_id("edtID")
 e.clear()
 e.send_keys(USER)
-e = browser.find_element_by_id("edtPass")
+e = driver.find_element_by_id("edtPass")
 e.clear()
 e.send_keys(PASS)
 # フォームを送信
-frm = browser.find_element_by_css_selector("#form1")
+frm = driver.find_element_by_css_selector("#form1")
 frm.submit()
 print("情報を入力してログインボタンを押しました")
 
 # マイページのURLを得る
-#a = browser.find_element_by_css_selector(".islogin a")
+#a = driver.find_element_by_css_selector(".islogin a")
 url_mypage = "https://1point02.jp/op/index.aspx"
 print("マイページのURL=", url_mypage)
 
@@ -35,7 +41,7 @@ print("マイページのURL=", url_mypage)
 browser.get(url_mypage)
 
 # お気に入りのタイトルを列挙
-links = browser.find_elements_by_css_selector(
+links = driver.find_elements_by_css_selector(
     "#gv_WAR_BAT li > a")
 for a in links:
     href = a.get_attribute('href')
