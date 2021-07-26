e302940636fcc710c33da43a8c31c5b939fcce5e
YamashitaTaiki <tai.tai.kinntyann.so@gmail.com>
2021-07-27 00:02:32 +0900

    chromeドライバー修正

--- selenium_test.py
+++ selenium_test.py
@@ -1,6 +1,9 @@
-from selenium import webdriver
-import sys
-from selenium.webdriver.chrome.options import Options
+from selenium import webdriver;
+import sys;
+import time
+import chromedriver_binary
+
+# ChromeOptionsを設定
 
 args = sys.argv
 USER = "tai.tai.kinntyann.so@gmail.com"
@@ -8,12 +11,16 @@ PASS = args[1]
 print(PASS)
 
 # ドライバーを得る
-options = webdriver.ChromeOptions()
-options.add_argument('--disable-gpu')
-options.add_argument("--no-sandbox")
-options.add_argument('--headless')
+# ChromeOptionsを設定
+options = webdriver.ChromeOptions();
+options.add_argument('--disable-gpu');
+options.add_argument('--disable-extensions')
+#options.add_argument('--proxy-server="direct://"')
+#options.add_argument('--proxy-bypass-list=*')
+#options.add_argument('--start-maximized')
+#options.add_argument('--kiosk')
+#options.add_argument('--headless');
 driver = webdriver.Chrome(options=options)
-driver.get('https://google.com')
 
 # ログインページにアクセス
 url_login = "https://1point02.jp/op/login.aspx"
@@ -21,19 +28,19 @@ driver.get(url_login)
 print("ログインページにアクセスしました")
 
 # テキストボックスに文字を入力
-e = driver.find_element_by_id("edtID")
-e.clear()
-e.send_keys(USER)
-e = driver.find_element_by_id("edtPass")
-e.clear()
-e.send_keys(PASS)
+id = driver.find_element_by_id("edtID")
+id.send_keys(USER)
+password = driver.find_element_by_id("edtPass")
+password.send_keys(PASS)
 # フォームを送信
 frm = driver.find_element_by_css_selector("#form1")
-frm.submit()
+
+login_button = driver.find_element_by_id("lbnLogin")
+login_button.click()
+
 print("情報を入力してログインボタンを押しました")
 
 # マイページのURLを得る
-#a = driver.find_element_by_css_selector(".islogin a")
 url_mypage = "https://1point02.jp/op/index.aspx"
 print("マイページのURL=", url_mypage)
 
