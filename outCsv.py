6a43253667b86ee41cc3f4a2e786ef637aa56d26
YamashitaTaiki <tai.tai.kinntyann.so@gmail.com>
2021-08-06 21:05:50 +0900

    スクレイピング修正

--- outCsv.py
+++ outCsv.py
@@ -24,7 +24,6 @@ driver.implicitly_wait(2)
 # サッカー統計画面に移動
 url_login = "https://www.premierleague.com/stats/top/players"
 driver.get(url_login)
-print("統計画面にアクセスしました。")
 
 # driver.find_element_by_id("top-team-stats-summary-grid")
 # ソースコードを取得
@@ -38,94 +37,86 @@ soup = BeautifulSoup(html, 'html.parser')  # または、'html.parser'
 
 
 # 1行目 Premier League Player Stats
-csvList = []
+csvList = [["position","kind","statName","statNameSecondary","stat","pos"]]
 
 # CSVファイル作成
-def createCsv(kind,num, css):
+def createCsv(position,kind,num, css):
     createList = []
     # json作成用リスト
     empty_list = []
     # 引数の項目を追加する
+    empty_list.append(position)
     empty_list.append(kind)
-    for i in soup.select(
-            css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li.statsHero > div.statInfo > a.statName'):
-        empty_list.append(i.get_text())
-        break
-    for i in soup.select(css + ' ul > li:nth-child('
-                         + str(num) + ') > div > ul > li.statsHero > div.statInfo > a.statNameSecondary'):
-        empty_list.append(i.get_text())
-        break
-    for i in soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li.statsHero > div.statInfo > div.stat'):
-        empty_list.append(str(i.get_text()).replace('\n', '').replace(' ',''))
-        break
-    for i in soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li.statsHero > div.statInfo > div.pos'):
-        empty_list.append(i.get_text())
-        break
+    statNameList = soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li.statsHero > div.statInfo > a.statName')
+    empty_list.append(statNameList[0].getText() if statNameList != [] else '')
+    statNameSecondaryList = soup.select(css + ' ul > li:nth-child('+ str(num) + ') > div > ul > li.statsHero > div.statInfo > a.statNameSecondary')
+    empty_list.append(statNameSecondaryList[0].getText() if statNameSecondaryList != [] else '')
+    statList = soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li.statsHero > div.statInfo > div.stat')
+    empty_list.append(str(statList[0].getText() if statList !=[] else '').replace('\n', '').replace(' ', ''))
+    posList = soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li.statsHero > div.statInfo > div.pos')
+    empty_list.append(posList[0].getText() if posList != [] else '')
+
     createList.append(empty_list)
 
     for index in range(2, 11):
         listData = []
-        for i in soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li:nth-child(' + str(
-                index) + ') > div.teamInfo > a.statName'):
-            listData.append(i.get_text())
-            break
-        for i in soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li:nth-child(' + str(
-                index) + ') > div.teamInfo > a.statNameSecondary'):
-            listData.append(i.get_text())
-            break
-        for i in soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li:nth-child(' + str(
-                index) + ') > div.stat'):
-            listData.append(str(i.get_text()).replace('\n', '').replace(' ',''))
-            break
-        for i in soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li:nth-child(' + str(
-                index) + ') > div.pos'):
-            listData.append(i.get_text())
+        statName = soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li:nth-child(' + str(index) + ') > div.teamInfo > a.statName')
             # 引数の項目を追加する
-            listData.append(kind)
-            break
-        if listData != []:
+        listData.append(position)
+        listData.append(kind)
+        listData.append(statName[0].getText() if statName != [] else '')
+        statNameSecondary = soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li:nth-child(' + str(
+                index) + ') > div.teamInfo > a.statNameSecondary')
+        listData.append(statNameSecondary[0].getText() if statNameSecondary != [] else '')
+        stat = soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li:nth-child(' + str(
+                index) + ') > div.stat')
+        listData.append(str(stat[0].getText() if stat != [] else '').replace('\n', '').replace(' ',''))
+        pos = soup.select(css + ' ul > li:nth-child(' + str(num) + ') > div > ul > li:nth-child(' + str(
+                index) + ') > div.pos')
+        listData.append(pos[0].getText() if pos != [] else '')
+        if listData[2] != '':
             createList.append(listData)
     return createList
 
 
 # Premier League Player Stats
-csvList = createCsv("Goal",1,
+csvList += createCsv("Premier League Player Stats","Goal",1,
                              '#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.\\32 01617PlayerStatsTopPlayers >')
-csvList += createCsv("Assists",2,
+csvList += createCsv("Premier League Player Stats","Assists",2,
                              '#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.\\32 01617PlayerStatsTopPlayers >')
-csvList += createCsv("Passes",3,
+csvList += createCsv("Premier League Player Stats","Passes",3,
                              '#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.\\32 01617PlayerStatsTopPlayers >')
-csvList += createCsv("Minuter player",4,
+csvList += createCsv("Premier League Player Stats","Minuter player",4,
                              '#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.\\32 01617PlayerStatsTopPlayers >')
 
 # Attack
-csvList += createCsv("shots",1,
+csvList += createCsv("Attack","shots",1,
                               "#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.AttackTopPlayers >")
-csvList += createCsv("hit woodwork",2,
+csvList += createCsv("Attack","hit woodwork",2,
                               "#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.AttackTopPlayers >")
-csvList += createCsv("through balls",3,
+csvList += createCsv("Attack","through balls",3,
                               "#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.AttackTopPlayers >")
-csvList += createCsv("crosses",4,
+csvList += createCsv("Attack","crosses",4,
                               "#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.AttackTopPlayers >")
 
 # Defence
-csvList += createCsv("tackles",1,
+csvList += createCsv("Defence","tackles",1,
                                 "#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.DefenceTopPlayers >")
-csvList += createCsv("blocks",2,
+csvList += createCsv("Defence","blocks",2,
                                 "#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.DefenceTopPlayers >")
-csvList += createCsv("clearances",3,
+csvList += createCsv("Defence","clearances",3,
                                 "#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.DefenceTopPlayers >")
-csvList += createCsv("head clearances",4,
+csvList += createCsv("Defence","head clearances",4,
                                 "#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.DefenceTopPlayers >")
 
 # Goalkeeper
-csvList += createCsv("Clean Sheets",1,
+csvList += createCsv("Goalkeeper","Clean Sheets",1,
                                 "#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.GoalkeeperTopPlayers >")
-csvList += createCsv("Saves",2,
+csvList += createCsv("Goalkeeper","Saves",2,
                                 "#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.GoalkeeperTopPlayers >")
-csvList += createCsv("Punches",3,
+csvList += createCsv("Goalkeeper","Punches",3,
                                 "#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.GoalkeeperTopPlayers >")
-csvList += createCsv("Goals Conceded",4,
+csvList += createCsv("Goalkeeper","Goals Conceded",4,
                                 "#mainContent > div.hasSideNav > div > div:nth-child(2) > section.mainWidget.statsRow.GoalkeeperTopPlayers >")
 
 # 辞書オブジェクトをJSONファイルへ出力
